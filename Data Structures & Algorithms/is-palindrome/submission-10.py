14dbf0e2e05a6096a490a5c446489139fef2f6a7
Yu-Ting Chen <105917993+yuting1008@users.noreply.github.com>
2026-06-24 16:23:06 +0800

    Add: is-palindrome - submission-10

---
+++ Data Structures & Algorithms/is-palindrome/submission-10.py
@@ -0,0 +1,24 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        if len(s) == 1:
+            return True
+
+        def is_alphanumeric(char):
+            ch = char.lower()
+            return ('a' <= ch <= 'z') or ('0' <= ch <= '9')
+        
+        s_updated_list = []
+        for char in s:
+            if is_alphanumeric(char):
+                s_updated_list.append(char.lower())
+            
+        s_updated = "".join(s_updated_list)
+        
+        left = 0
+        right = len(s_updated) - 1
+        while left <= right:
+            if s_updated[left] != s_updated[right]:
+                return False
+            left += 1
+            right -= 1
+        return True
