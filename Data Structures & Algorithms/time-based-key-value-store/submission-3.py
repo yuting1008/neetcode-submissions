f30023f5746c6b4716a170f6faf13ad4001fc503
Yu-Ting Chen <105917993+yuting1008@users.noreply.github.com>
2026-07-24 15:07:20 +0800

    Add: time-based-key-value-store - submission-3

---
+++ Data Structures & Algorithms/time-based-key-value-store/submission-3.py
@@ -0,0 +1,24 @@
+class TimeMap:
+
+    def __init__(self):
+        self.mood = defaultdict(list)
+
+    def set(self, key: str, value: str, timestamp: int) -> None:
+        self.mood[key].append((timestamp, value))
+
+    def get(self, key: str, timestamp: int) -> str:
+        pairs = self.mood[key]
+        l, r = 0, len(pairs) - 1
+        res = ""
+        while l <= r:
+            m = l + ((r - l) // 2)
+            # if timestamp == pairs[m][0]:
+            #     return pairs[m][1]
+            # if timestamp > pairs[r][0]:
+            #     return pairs[r][1]
+            if timestamp < pairs[m][0]:
+                r = m - 1
+            elif timestamp >= pairs[m][0]:
+                l = m + 1
+                res = pairs[m][1]
+        return res
